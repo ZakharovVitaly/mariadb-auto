8278c6eb268b02e4d5e1375abe92c91619eed0bf
ZakharovVitaly <44725832+ZakharovVitaly@users.noreply.github.com>
2018-11-15 13:39:48 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 starttime=`date +%s`
-for x in `cat hosts | egrep  -o '^\s*[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'`; do ssh-keygen -R $x -f /`echo $HOME`/.ssh/known_hosts; ssh -o "StrictHostKeyChecking no" -o "PasswordAuthentication no" $x; done
+for x in `cat hosts | egrep  -o '^\s*[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'`; do ssh-keygen -R $x -f $HOME/.ssh/known_hosts; ssh -o "StrictHostKeyChecking no" -o "PasswordAuthentication no" $x; done
 ansible-playbook -i hosts mariadb.yml
 finishtime=`date +%s`
 printf 'Seconds:'
